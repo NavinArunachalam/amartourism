d131729ed6d78bc7e34103b680a3050feb58efc9
NavinArunachalam <navinarunachalam20@gmail.com>
2025-10-15 13:44:45 +0530

    price

--- client/src/components/HoneymoonSection.jsx
+++ client/src/components/HoneymoonSection.jsx
@@ -94,7 +94,7 @@ const HoneymoonSection = () => {
         </div>
 
         {/* Honeymoon Destinations Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
           {honeymoonDestinations.map((destination, index) => (
             <Card
               key={destination.id}
--- client/src/components/PackagesSection.jsx
+++ client/src/components/PackagesSection.jsx
@@ -107,7 +107,7 @@ const PackagesSection = () => {
         </div>
 
         {/* Packages Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {packages.map((pkg, index) => (
             <Card
               key={pkg.id}
